1230e30c7516675f33d3483da4707b040fd88dfa
Tom Portugez <chip_luxury@gmail.com>
2022-06-07 21:38:41 +0300

    feat: add google calendar maintenance event

---
+++ googleAppsScript.js
@@ -0,0 +1,18 @@
+// this code is a work in progress, it main feature is to add google calendar maintenance event:
+
+function addevent(status, location, type, power) {
+    //setting:
+    var SS = SpreadsheetApp.getActive();
+    var sheet = SS.getSheetByName("DB");
+    var cal = CalendarApp.getCalendarById(
+        "remove-my-calander-id-for-github@group.calendar.google.com"
+    );
+
+    var options = {
+        description: " סוג הנורה: " + type + " בהספק של: " + power,
+        location: location,
+    };
+    if (status == "cheak report") {
+        cal.createAllDayEvent("תיקון תאורה:", new Date(), options);
+    }
+}
